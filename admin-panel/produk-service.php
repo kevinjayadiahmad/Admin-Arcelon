a761bcea73f53d6a05ee3ff5942fef479f509777
Kevin Jayadi Ahmad <jyadiahmad@gmail.com>
2020-07-23 14:05:48 +0700

    Update produk-service.php

--- admin-panel/produk-service.php
+++ admin-panel/produk-service.php
@@ -45,20 +45,6 @@
             <!--logo start-->
             <a href="index.php" class="logo"><b>ARC<span>ELON</span></b></a>
             <!--logo end-->
-            <div class="nav notify-row" id="top_menu">
-                <!--  notification start -->
-                <ul class="nav top-menu">
-                    <!-- settings start -->
-                    <!-- settings end -->
-                    <!-- inbox dropdown start-->
-
-                    <!-- inbox dropdown end -->
-                    <!-- notification dropdown start-->
-
-                    <!-- notification dropdown end -->
-                </ul>
-                <!--  notification end -->
-            </div>
             <div class="top-menu">
                 <ul class="nav pull-right top-menu">
                     <li><a class="logout" href="login.html">Logout</a></li>
@@ -153,12 +139,6 @@
                             </form>
                         </div>
                     </div>
-                    <!-- /form-panel -->
-                    </div>
-                    <!-- /col-lg-12 -->
-                    </div>
-                    <!-- /row -->
-                    <!-- INPUT MESSAGES -->
                 </section>
             </section>
         </section>
@@ -200,48 +180,6 @@
     <!--script for this page-->
     <script src="lib/sparkline-chart.js"></script>
     <script src="lib/zabuto_calendar.js"></script>
-    <script type="application/javascript">
-        $(document).ready(function() {
-            $("#date-popover").popover({
-                html: true,
-                trigger: "manual"
-            });
-            $("#date-popover").hide();
-            $("#date-popover").click(function(e) {
-                $(this).hide();
-            });
-
-            $("#my-calendar").zabuto_calendar({
-                action: function() {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function() {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [{
-                        type: "text",
-                        label: "Special event",
-                        badge: "00"
-                    },
-                    {
-                        type: "block",
-                        label: "Regular event",
-                    }
-                ]
-            });
-        });
-
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-    </script>
 </body>
 
 </html>
