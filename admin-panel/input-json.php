05d19974c7de41d3eb70480b6d7e8202e303c16e
Kevin Jayadi Ahmad <jyadiahmad@gmail.com>
2020-07-23 14:05:43 +0700

    Update input-json.php

--- admin-panel/input-json.php
+++ admin-panel/input-json.php
@@ -144,18 +144,16 @@
                         <br>
                         <br>
                         <br>
-                        <b>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                        </b>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
                 </section>
             </section>
         </section>
@@ -197,48 +195,6 @@
     <!--script for this page-->
     <script src="lib/sparkline-chart.js"></script>
     <script src="lib/zabuto_calendar.js"></script>
-    <script type="application/javascript">
-        $(document).ready(function() {
-            $("#date-popover").popover({
-                html: true,
-                trigger: "manual"
-            });
-            $("#date-popover").hide();
-            $("#date-popover").click(function(e) {
-                $(this).hide();
-            });
-
-            $("#my-calendar").zabuto_calendar({
-                action: function() {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function() {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [{
-                        type: "text",
-                        label: "Special event",
-                        badge: "00"
-                    },
-                    {
-                        type: "block",
-                        label: "Regular event",
-                    }
-                ]
-            });
-        });
-
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-    </script>
 </body>
 
 </html>
