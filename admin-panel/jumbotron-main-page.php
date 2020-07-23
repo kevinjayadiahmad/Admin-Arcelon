8fa8b838cf84efb7e70beceebee8a195872576a7
Kevin Jayadi Ahmad <jyadiahmad@gmail.com>
2020-07-23 11:05:43 +0700

    Update jumbotron-main-page.php

--- admin-panel/jumbotron-main-page.php
+++ admin-panel/jumbotron-main-page.php
@@ -1,3 +1,20 @@
+<?php
+//koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "adminarcelon");
+//cek apakah tombol submit sudah di tekan atau belum
+if (isset($_POST["submit"])) {
+    //ambil data dari tiap elemen dalam form
+    $tittle = $_POST["tittle"];
+    $deskripsi = $_POST["deskripsi"];
+    $gambar = $_POST["gambar"];
+
+    //query insert data
+    $query = "INSERT INTO mainpagehome
+                VALUES
+                ('', '$tittle', '$deskripsi', '$gambar')";
+    mysqli_query($conn, $query);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -46,18 +63,6 @@
             <a href="index.php" class="logo"><b>ARC<span>ELON</span></b></a>
             <!--logo end-->
             <div class="nav notify-row" id="top_menu">
-                <!--  notification start -->
-                <ul class="nav top-menu">
-                    <!-- settings start -->
-                    <!-- settings end -->
-                    <!-- inbox dropdown start-->
-
-                    <!-- inbox dropdown end -->
-                    <!-- notification dropdown start-->
-
-                    <!-- notification dropdown end -->
-                </ul>
-                <!--  notification end -->
             </div>
             <div class="top-menu">
                 <ul class="nav pull-right top-menu">
@@ -88,77 +93,76 @@
         MAIN CONTENT
         *********************************************************************************************************************************************************** -->
         <!--main content start-->
-        <section id="main-content">
-            <section class="wrapper">
+        <form action="" method="post">
+            <section id="main-content">
                 <section class="wrapper">
-                    <h3><i class="fa fa-angle-right"></i> Form Jumbotron Main Page Home</h3>
-                    <!-- BASIC FORM ELELEMNTS -->
-                    <div class="row mt">
-                        <div class="col-lg-12">
-                            <div class="form-panel">
-                                <h4 class="mb"><i class="fa fa-angle-right"></i> Form</h4>
-                                <form class="form-horizontal style-form" method="get">
-                                    <div class="form-group">
-                                        <label class="col-sm-2 col-sm-2 control-label">Tittle</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" class="form-control">
-                                            <span class="help-block">Ini Adalah Form Untuk Mengganti Tittle Di Jumbotron Slide Pertama</span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 col-sm-2 control-label">Deskripsi</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" class="form-control">
-                                            <span class="help-block">Ini Adalah Form Untuk Mengganti Deskripsi Di Jumbotron Slide Pertama</span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Jumbotron Photo</label>
-                                        <div class="controls col-md-9">
-                                            <div class="fileupload fileupload-new" data-provides="fileupload">
-                                                <span class="btn btn-theme02 btn-file">
-                                                    <span class="fileupload-new"><i class="fa fa-paperclip"></i> Select file</span>
-                                                    <input type="file" class="default" />
-                                                </span>
-                                                <span class="fileupload-preview" style="margin-left:5px;"></span>
-                                                <a href="advanced_form_components.html#" class="close fileupload-exists" data-dismiss="fileupload" style="float: none; margin-left:5px;"></a>
+                    <section class="wrapper">
+                        <h3><i class="fa fa-angle-right"></i> Form Jumbotron Main Page Home</h3>
+                        <!-- BASIC FORM ELELEMNTS -->
+                        <div class="row mt">
+                            <div class="col-lg-12">
+                                <div class="form-panel">
+                                    <h4 class="mb"><i class="fa fa-angle-right"></i> Form</h4>
+                                    <form class="form-horizontal style-form">
+                                        <div class="form-group">
+                                            <label class="col-sm-2 col-sm-2 control-label" for="tittle">Tittle</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" name="tittle" id="tittle">
+                                                <span class="help-block">Ini Adalah Form Untuk Mengganti Tittle Di Jumbotron Slide Pertama</span>
                                             </div>
                                         </div>
-                                    </div>
-                                    <button type="button" class="btn btn-primary btn-lg">Submit</button>
-                                </form>
-                            </div>
-                            <!-- /form-panel -->
-                            </form>
-                        </div>
-                    </div>
-                    <!-- col-lg-12-->
-                    </div>
-                </section>
-            </section>
-        </section>
-        <!--main content end-->
-        <!--footer start-->
-        <footer class="site-footer">
-            <div class="text-center">
-                <p>
-                    &copy; Copyrights <strong>Dashio</strong>. All Rights Reserved
-                </p>
-                <div class="credits">
-                    <!--
+                                        <div class="form-group">
+                                            <label class="col-sm-2 col-sm-2 control-label" for="deskripsi">Deskripsi</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" name="deskripsi" id="deskripsi">
+                                                <span class="help-block">Ini Adalah Form Untuk Mengganti Deskripsi Di Jumbotron Slide Pertama</span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-2 col-sm-2 control-label" for="gambar">Gambar</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" name="gambar" id="gambar">
+                                                <span class="help-block">Ini Adalah Form Untuk Mengganti Gambar Di Jumbotron Slide Pertama</span>
+                                            </div>
+                                        </div>
+                                        <button type="submit" name="submit" class="btn btn-primary btn-lg">Submit</button>
+                                    </form>
+                                </div>
+                                <!-- /form-panel -->
+        </form>
+        </div>
+        </div>
+        <!-- col-lg-12-->
+        </div>
+    </section>
+    </section>
+    </section>
+    <br>
+    <br>
+    <br>
+    <br>
+    <!--main content end-->
+    <!--footer start-->
+    <footer class="site-footer">
+        <div class="text-center">
+            <p>
+                &copy; Copyrights <strong>Dashio</strong>. All Rights Reserved
+            </p>
+            <div class="credits">
+                <!--
             You are NOT allowed to delete the credit link to TemplateMag with free version.
             You can delete the credit link only if you bought the pro version.
             Buy the pro version with working PHP/AJAX contact form: https://templatemag.com/dashio-bootstrap-admin-template/
             Licensing information: https://templatemag.com/license/
           -->
-                    Created with Dashio template by <a href="https://templatemag.com/">TemplateMag</a>
-                </div>
-                <a href="index.html#" class="go-top">
-                    <i class="fa fa-angle-up"></i>
-                </a>
+                Created with Dashio template by <a href="https://templatemag.com/">TemplateMag</a>
             </div>
-        </footer>
-        <!--footer end-->
+            <a href="index.html#" class="go-top">
+                <i class="fa fa-angle-up"></i>
+            </a>
+        </div>
+    </footer>
+    <!--footer end-->
     </section>
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="lib/jquery/jquery.min.js"></script>
@@ -175,48 +179,6 @@
     <!--script for this page-->
     <script src="lib/sparkline-chart.js"></script>
     <script src="lib/zabuto_calendar.js"></script>
-    <script type="application/javascript">
-        $(document).ready(function() {
-            $("#date-popover").popover({
-                html: true,
-                trigger: "manual"
-            });
-            $("#date-popover").hide();
-            $("#date-popover").click(function(e) {
-                $(this).hide();
-            });
-
-            $("#my-calendar").zabuto_calendar({
-                action: function() {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function() {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [{
-                        type: "text",
-                        label: "Special event",
-                        badge: "00"
-                    },
-                    {
-                        type: "block",
-                        label: "Regular event",
-                    }
-                ]
-            });
-        });
-
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-    </script>
 </body>
 
 </html>
